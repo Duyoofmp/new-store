688c30f601850f360cf721656232cada734b1902
duyoof <duyoofibnudileep@gmail.com>
2020-10-10 12:52:18 +0530

    web dev learn

---
+++ config/collections.js
@@ -0,0 +1,7 @@
+//const userHelpers = require("../helpers/user-helpers");idhaayirunnu thett (error)
+
+module.exports={
+    PRODUCT_COLLECTION:'product',
+    USER_COLLECTION:'user',
+    CART_COLLECTION:'cart'
+}
---
+++ helpers/product-helpers.js
@@ -0,0 +1,56 @@
+var db=require('../config/connection')
+var collection=require('../config/collections')
+const { response } = require('express')
+var objectId=require('mongodb').ObjectID
+module.exports={
+
+    addProduct:(product,callback)=>{
+        
+
+        db.get().collection('product').insertOne(product).then((data)=>{
+            
+            callback(data.ops[0]._id)
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+
+    },
+    deleteProduct:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).removeOne({_id:objectId(proId) }).then((response)=>{
+            resolve(response)
+            })
+        })
+    },
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId) }).then((product)=>{
+                resolve(product)
+            })
+
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(proId)},{
+              $set:{
+                  Name:proDetails.Name,
+                  Category:proDetails.Category,
+                  Price:proDetails.Price,
+                  Description:proDetails.Description
+
+              }
+            }).then((response)=>{
+                resolve()
+            })
+
+        })
+    }
+    
+        
+}
